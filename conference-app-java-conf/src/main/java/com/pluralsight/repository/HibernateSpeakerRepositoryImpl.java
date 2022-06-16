ed8200a8c74517edcf05850af7e260c0e0a14076
Roshan Veerabhadraiah <veerabha@synopsys.com>
2022-06-16 13:05:25 +0530

    worked on spring factory bean

--- conference-app-java-conf/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ conference-app-java-conf/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,15 +1,21 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import com.pluralsight.util.CalendarFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 // all db crud operation
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    Calendar calendar;
 
     @Override
     public List<Speaker> findAll() {
@@ -20,6 +26,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setLastName("v");
 
         speakers.add(speaker);
+
+        System.out.println("cal : " +  calendar.getTime());
 
         return speakers;
     }
