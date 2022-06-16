1cc2d86fd07e3d9cabf49fe74f2cc4e02ba30735
Roshan Veerabhadraiah <veerabha@synopsys.com>
2022-06-16 17:12:34 +0530

    learn bean profile

--- conference-app-java-conf/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ conference-app-java-conf/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.pluralsight.model.Speaker;
 import com.pluralsight.util.CalendarFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.List;
 // all db crud operation
 
 @Repository("speakerRepository")
+//@Profile("dev")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
     @Autowired
